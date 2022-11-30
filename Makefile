77ebbebb1ce78483949f25f02f6de47d6dd4baf8
Sidney <segoldinger@wpi.edu>
2022-11-30 16:37:46 -0500

    command line working

--- Makefile
+++ Makefile
@@ -3,7 +3,6 @@ overlay: overlay.cpp
 
 all: overlay.cpp
 	g++ -o overlay overlay.cpp
-	# -std=c++11 maybe?
 
 clean:
 	$(RM) overlay
--- overlay.cpp
+++ overlay.cpp
@@ -2,3 +2,25 @@
 // Created by Sidney Goldinger on 11/30/22.
 //
 
+#import <stdio.h>
+#include <iostream>
+#include <string_view>
+
+void router() {
+    printf("router\n");
+}
+
+void endhost() {
+    printf("host\n");
+}
+
+int main (int argc, char **argv) {
+    // test if end-host or router
+    std::string arg1 = argv[1];
+
+    if (arg1 == "r") { router(); }
+    else if (arg1 == "h") { endhost(); }
+    else { printf("please input r or h.\n"); return 0; }
+
+    return 0;
+}
