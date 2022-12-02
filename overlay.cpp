148f1a0aad780c16bdce7b2bfdf7d97a4ce15b23
Connor <codingconrad49@gmail.com>
2022-12-02 14:24:27 -0500

    Started code

--- overlay.cpp
+++ overlay.cpp
@@ -2,9 +2,15 @@
 // Created by Sidney Goldinger on 11/30/22.
 //
 
-#import <stdio.h>
+#include <stdio.h>
 #include <iostream>
 #include <string_view>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
 
 void router() {
     printf("router\n");
@@ -23,4 +29,55 @@ int main (int argc, char **argv) {
     else { printf("please input r or h.\n"); return 0; }
 
     return 0;
+}
+
+// Set the following port to a unique number:
+#define MYPORT 5950
+
+int create_cs3516_socket() {
+    int sock;
+    struct sockaddr_in server;
+    
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if (sock < 0) error("Error creating CS3516 socket");
+
+    bzero(&server, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(MYPORT);
+    if (bind(sock, (struct sockaddr *) &server, sizeof(server) ) < 0) 
+        error("Unable to bind CS3516 socket");
+
+    // Socket is now bound:
+    return sock;
+}
+
+int cs3516_recv(int sock, char *buffer, int buff_size) {
+    struct sockaddr_in from;
+    int fromlen, n;
+    fromlen = sizeof(struct sockaddr_in);
+    n = recvfrom(sock, buffer, buff_size, 0,
+                 (struct sockaddr *) &from, &fromlen);
+
+    return n;
+}
+
+int cs3516_send(int sock, char *buffer, int buff_size, unsigned long nextIP) {
+    struct sockaddr_in to;
+    int tolen, n;
+
+    tolen = sizeof(struct sockaddr_in);
+
+    // Okay, we must populate the to structure. 
+    bzero(&to, sizeof(to));
+    to.sin_family = AF_INET;
+    to.sin_port = htons(MYPORT);
+    to.sin_addr.s_addr = nextIP;
+
+    // We can now send to this destination:
+    n = sendto(sock, buffer, buff_size, 0,
+               (struct sockaddr *)&to, tolen);
+
+    return n;
 }
